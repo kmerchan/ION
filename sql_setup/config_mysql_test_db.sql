509fc013da45cf2117f308d5104200235699c53b
Kelsie Merchant <eislek02@gmail.com>
2020-10-15 19:26:29 +0000

    Add Bash script and sql setup files to add databases and users for ION main production and testing

---
+++ sql_setup/config_mysql_test_db.sql
@@ -0,0 +1,9 @@
+-- This script sets up the database and user for ION testing
+-- User is identified by password and has privileges
+
+CREATE DATABASE IF NOT EXISTS ion_test_db;
+DROP USER IF EXISTS 'ion_test'@'localhost';
+CREATE USER IF NOT EXISTS 'ion_test'@'localhost' IDENTIFIED BY 'ion_test_pwd';
+GRANT ALL PRIVILEGES ON ion_main_db.* TO 'ion_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'ion_test'@'localhost';
+FLUSH PRIVILEGES;
