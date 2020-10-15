56ae08fea001c849a602c5b3aa66cc5b54e3bf62
Kelsie Merchant <eislek02@gmail.com>
2020-10-15 20:54:42 +0000

    Add script to run all or specific unit tests after setting environment variables to call test database

---
+++ run_tests.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# Script to run unit tests with test db environment variables set
+
+ION_MYSQL_HOST=localhost
+ION_MYSQL_USER=ion_test
+ION_MYSQL_PWD=ion_test_pwd
+ION_MYSQL_DB=ion_test_db
+if [ $# -eq 1 ]; then
+    python3 -m unittest $1
+else
+    python3 -m unittest discover tests
+fi
